0954e9ac7fcb894e4ccf37ddebfe44ba15c2202f
mdx <mosesk.wamae@gmail.com>
2022-05-20 04:46:54 +0300

    edit to title

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = "Wamae 'mdx' Moses"
-export const siteTitle ="Next.js sample website"
+export const siteTitle ="ByDesign"
 
 export default function Layout({children,home}){
 
