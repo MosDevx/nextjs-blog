6df209d5250ccc95bf4e0aed11043a69eef49025
mdx <mosesk.wamae@gmail.com>
2022-05-20 03:41:13 +0300

    the second commit

--- pages/profile-pic.js
+++ pages/profile-pic.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
 
-export const ProfilePic = () => (
+export function ProfilePic() {}
+	return(
 	<Image src="/images/profile.jpg"
 		height={144}
 		width={144}
 		alt="Moses Wamae"
 	/>
-
-)
+	)
+}
 
